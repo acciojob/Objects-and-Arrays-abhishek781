1e977b863af1c3bb1d5ced194b85e0a87f05c5cc
abhishek781 <55807175+abhishek781@users.noreply.github.com>
2023-03-25 12:51:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const person = {
       age: 80
     };
 
+
+// create a reference to players array
+let team = players;
+
+// create a copy of players array
+let team1 = players.slice();
+
+// create a copy of person object
+let cap1 = Object.assign({}, person);
+
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 
